22ee9d2423781ff9e51af77ddd92178333d41104
wayne.smith <>
2019-03-04 15:17:52 +0000

    new module

--- packages/helloWorld/index.js
+++ packages/helloWorld/index.js
@@ -2,6 +2,7 @@
 
 const hello = require('sage-myget-hello');
 const world = require('sage-myget-world');
+const temp = require('sage-myget-temp');
 
-console.log(`${hello()}${world()}`);
+console.log(`${hello()}${world()}${temp()}`);
 

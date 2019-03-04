c1238fbc7e641af6dee38e92eff5e1763ac860da
wayne.smith <>
2019-03-04 14:25:26 +0000

    test

--- packages/hello/index.js
+++ packages/hello/index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = () => 'hello';
+module.exports = () => 'hello!';
--- packages/world/index.js
+++ packages/world/index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = () => 'world';
+module.exports = () => 'world!';

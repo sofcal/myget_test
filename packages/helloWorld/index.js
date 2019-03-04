6e97d927ea7b077332dcb73dde7e68dd9f0a2431
wayne.smith <>
2019-03-04 15:26:38 +0000

    cne

--- packages/helloWorld/index.js
+++ packages/helloWorld/index.js
@@ -3,6 +3,7 @@
 const hello = require('sage-myget-hello');
 const world = require('sage-myget-world');
 const temp = require('sage-myget-temp');
+const temp2 = require('sage-myget-temp2');
 
-console.log(`${hello()}${world()}${temp()}`);
+console.log(`${hello()}${world()}${temp()}${temp2()}`);
 
